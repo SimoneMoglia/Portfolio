f37c06a41f033341432b4db15d75841430d2bc1d
Simone Moglia <moglia.simone96@gmail.com>
2020-10-12 16:32:58 +0200

    mail3

--- mail.php
+++ mail.php
@@ -1,40 +1,19 @@
 <?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
+if(isset($_POST['submit'])){
+    $to = "moglia.simone96@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-
-if ($name === ''){
-echo "Il nome non può essere vuoto.";
-die();
-}
-if ($email === ''){
-echo "L'email non può essere vuota.";
-die();
-} else {
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Formato email non valido.";
-die();
-}
-}
-if ($subject === ''){
-echo "L'oggetto non può essere vuoto.";
-die();
-}
-if ($message === ''){
-echo "Il messaggio non può essere vuoto.";
-die();
-}
-}
-
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "moglia.simone96@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Errore!");
-echo "Messaggio Inviato!";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
