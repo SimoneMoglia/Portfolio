b9bfb14e263e5e826052bb011a247e6a8da34055
Simone Moglia <moglia.simone96@gmail.com>
2020-10-14 16:09:06 +0200

    mailtry

---
+++ invia-email.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($_POST["submit"]))
+{
+  $nome=trim(strip_tags($_POST["nome"]));
+  $email=trim(strip_tags($_POST["email"]));
+  $oggetto=trim(strip_tags($_POST["oggetto"]));
+  $messaggio=trim(strip_tags($_POST["messaggio"]));
+
+  $header = "Da $email" . "Nome: $nome";
+  $subject = "$oggetto";
+  $indirizzo = "moglia.simone96@gmail.com";
+
+
+
+  if(mail($indirizzo, $subject, $messaggio, $header)) {
+    echo "Email inviata correttamente";
+  }
+  else {
+    echo "Errore durante l'invio";
+  }
+}
+
+ ?>
